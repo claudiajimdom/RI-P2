31edf0046b72a1c3e40a899b0d52599187bb2f97
Claudia <claudiajimdom@correo.ugr.es>
2025-10-21 17:42:05 +0200

    emoticonos arreglados

--- src/Xanalyzer.java
+++ src/Xanalyzer.java
@@ -15,11 +15,14 @@ public class Xanalyzer extends Analyzer {
 
     @Override
     protected TokenStreamComponents createComponents(String fieldName) {
-        // tokens validos (@cadena , #cadena)
-        Pattern cadena = Pattern.compile("\\s+"); // separa por espacio
-        Tokenizer tokenizer = new PatternTokenizer(cadena, -1);
+        // Patrón que captura: emojis completos (con variantes), palabras, @menciones, #hashtags
+        Pattern tokenPattern = Pattern.compile(
+            "[\\p{So}\\p{Sk}\\p{Sm}][\\p{M}\\uFE0F\\u200D]*" + // Emojis con modificadores y variantes
+            "|[@#]?[\\p{L}\\p{N}_]+"  // Palabras, @menciones, #hashtags
+        );
+        Tokenizer tokenizer = new PatternTokenizer(tokenPattern, 0);
 
-        // Filtro lowercase 
+        // Filtro lowercase (los emojis no se afectan)
         TokenStream tokenStream = new LowerCaseFilter(tokenizer);
 
         // Filtro de sinónimos para emoticonos
@@ -37,24 +40,27 @@ public class Xanalyzer extends Analyzer {
     private SynonymMap buildEmojiSynonyms() throws IOException, ParseException {
         SynonymMap.Builder builder = new SynonymMap.Builder(true);
 
-
+        // Emojis básicos sin variantes
         builder.add(new CharsRef("😊"), new CharsRef("feliz"), true);
-        builder.add(new CharsRef("😡​"), new CharsRef("enfadado"), true);
+        builder.add(new CharsRef("😡"), new CharsRef("enfadado"), true);
         builder.add(new CharsRef("😢"), new CharsRef("triste"), true);
         builder.add(new CharsRef("😂"), new CharsRef("risa"), true);
         builder.add(new CharsRef("😍"), new CharsRef("enamorado"), true);
         builder.add(new CharsRef("😎"), new CharsRef("guay"), true);
         builder.add(new CharsRef("😴"), new CharsRef("cansado"), true);
         builder.add(new CharsRef("🤔"), new CharsRef("pensativo"), true);
-        builder.add(new CharsRef("😱​"), new CharsRef("asombrado"), true);
-        builder.add(new CharsRef("👌​"), new CharsRef("perfecto"), true);
+        builder.add(new CharsRef("😱"), new CharsRef("asombrado"), true);
+        builder.add(new CharsRef("👌"), new CharsRef("perfecto"), true);
         builder.add(new CharsRef("👍"), new CharsRef("bien"), true);
         builder.add(new CharsRef("👎"), new CharsRef("mal"), true);
         builder.add(new CharsRef("🙏"), new CharsRef("gracias"), true);
         builder.add(new CharsRef("💪"), new CharsRef("fuerza"), true);
         builder.add(new CharsRef("🔥"), new CharsRef("genial"), true);
-        builder.add(new CharsRef("❤️"), new CharsRef("amor"), true);
-        builder.add(new CharsRef("😱\uFE0F"), new CharsRef("asombrado"), true);
+        builder.add(new CharsRef("❤"), new CharsRef("amor"), true);
+        
+        // Variantes con presentation selector (U+FE0F)
+        builder.add(new CharsRef("❤\uFE0F"), new CharsRef("amor"), true);
+        
         return builder.build();
     }
 }
